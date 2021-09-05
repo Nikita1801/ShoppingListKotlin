e814a685ce1757d4fbc66092a7a5c64d459cc5fa
Nikita <nikita-makarevich@mail.ru>
2021-09-05 23:01:41 +0300

    lesson 8 (fix recycleview)

--- app/src/main/java/com/example/shoppinglistkotlin/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/example/shoppinglistkotlin/data/ShopListRepositoryImpl.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import com.example.shoppinglistkotlin.domain.ShopItem
 import com.example.shoppinglistkotlin.domain.ShopListRepository
 import java.lang.RuntimeException
+import kotlin.random.Random
 
 object ShopListRepositoryImpl: ShopListRepository {
 
@@ -15,8 +16,8 @@ object ShopListRepositoryImpl: ShopListRepository {
 
 
     init{
-        for (i  in 0 until 10) {
-            val item = ShopItem("Name $i", i, true)
+        for (i  in 0 until 1000) {
+            val item = ShopItem("Name $i", i, Random.nextBoolean())
             addShopItem(item)
         }
     }
